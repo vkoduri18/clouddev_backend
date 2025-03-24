f895ff95a3c48d76c07121a4d53c7c0034c44e75
Avanti <kantumuchuavanthi@gmail.com>
2025-03-24 15:45:26 -0500

    Initial commit - Node backend with MySQL

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Local MySQL connection config
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'avanti',
+  database: 'studentdb',
+  port: 3306,
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL database');
+});
+
+app.get('/api/students', (req, res) => {
+  db.query('SELECT * FROM students', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
